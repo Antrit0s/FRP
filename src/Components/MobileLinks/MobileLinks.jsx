3f55fe469ec8dc7551750c0edccb2b79439e5e5c
Antrit0s <yosefmohsen06@gmail.com>
2024-08-04 22:40:54 +0300

    navmenu

--- src/Components/MobileLinks/MobileLinks.jsx
+++ src/Components/MobileLinks/MobileLinks.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function DesktopLinks() {
   return (
-    <div className="links ">
+    <div className="links md:hidden ">
       <ul className="">
         <li className="hover:bg-green-300 p-2 rounded">
           <NavLink to="about">About</NavLink>
